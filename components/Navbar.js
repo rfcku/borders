84886c33e8da2eabf94f87faea11b315397da7cc
rfcku <rafackyou@gmail.com>
2026-01-04 14:46:12 -0800

    updates

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,44 +1,73 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import TagsInput from 'react-tagsinput'
+import TagsInput from "react-tagsinput";
 
-export const Navbar = ({ handleInput, date, time, tags }) => {
+export const Navbar = ({ handleInput, date, time, timeago, tags }) => {
   return (
-    <div className='flex flex-row gap-2 px-20 py-10 justify-between align-middle items-center'>
-      <Link href="/" className='flex flex-row gap-3 align-middle items-center'>
-
-
-
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-14">
-          <path fillRule="evenodd" d="M9.315 7.584C12.195 3.883 16.695 1.5 21.75 1.5a.75.75 0 0 1 .75.75c0 5.056-2.383 9.555-6.084 12.436A6.75 6.75 0 0 1 9.75 22.5a.75.75 0 0 1-.75-.75v-4.131A15.838 15.838 0 0 1 6.382 15H2.25a.75.75 0 0 1-.75-.75 6.75 6.75 0 0 1 7.815-6.666ZM15 6.75a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5Z" clipRule="evenodd" />
-          <path d="M5.26 17.242a.75.75 0 1 0-.897-1.203 5.243 5.243 0 0 0-2.05 5.022.75.75 0 0 0 .625.627 5.243 5.243 0 0 0 5.022-2.051.75.75 0 1 0-1.202-.897 3.744 3.744 0 0 1-3.008 1.51c0-1.23.592-2.323 1.51-3.008Z" />
-        </svg>
-
-
-
-        <div className='flex flex-col'>
-          <h1 className='font-extrabold text-2xl'>GoBorderGo</h1>
-          <div className='text-xs dark:text-zinc-400 font-light'>{date}</div>
-        </div>
-      </Link>
-      <div className='flex flex-row gap-3'>
-        <button className='text-zinc-500'>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+    <div className="sticky top-0 z-50 p-0 bg-zinc-950">
+      <div className="flex flex-row gap-2 px-20 py-4 justify-between align-middle items-center">
+        <Link
+          href="/"
+          className="flex flex-row gap-3 align-middle items-center"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-14"
+          >
+            <path
+              fillRule="evenodd"
+              d="M9.315 7.584C12.195 3.883 16.695 1.5 21.75 1.5a.75.75 0 0 1 .75.75c0 5.056-2.383 9.555-6.084 12.436A6.75 6.75 0 0 1 9.75 22.5a.75.75 0 0 1-.75-.75v-4.131A15.838 15.838 0 0 1 6.382 15H2.25a.75.75 0 0 1-.75-.75 6.75 6.75 0 0 1 7.815-6.666ZM15 6.75a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5Z"
+              clipRule="evenodd"
+            />
+            <path d="M5.26 17.242a.75.75 0 1 0-.897-1.203 5.243 5.243 0 0 0-2.05 5.022.75.75 0 0 0 .625.627 5.243 5.243 0 0 0 5.022-2.051.75.75 0 1 0-1.202-.897 3.744 3.744 0 0 1-3.008 1.51c0-1.23.592-2.323 1.51-3.008Z" />
           </svg>
 
-        </button>
-        <TagsInput
-          value={tags}
-          onChange={handleInput}
-          className='flex flex-row gap-2 bg-zinc-900 px-2 py-1 rounded-md items-center align-middle'
-          inputProps={{
-            placeholder: 'Search',
-          }}
-        />
+          <div className="flex flex-col">
+            <h1 className="font-extrabold text-2xl">GoBorderGo</h1>
+            <div className="text-xs dark:text-zinc-400 font-light">{date}</div>
+          </div>
+        </Link>
+        <div className="flex flex-row gap-3">
+          <div className="flex flex-col items-end">
+            <code className="text-sm font-light">
+              Updated: {date} {time}
+            </code>
+            <code className="text-sm font-light">{timeago}</code>
+          </div>
+
+          <button className="text-zinc-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+              />
+            </svg>
+          </button>
+          <TagsInput
+            value={tags}
+            onChange={handleInput}
+            className="flex flex-row gap-2 bg-zinc-900 px-2 py-1 rounded-md items-center align-middle min-w-[250px]"
+            inputProps={{
+              placeholder: "Search",
+            }}
+          />
+        </div>
       </div>
     </div>
   );
 };
-
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,22 @@
-import { useState, useEffect } from 'react';
-import Head from 'next/head';
-import Border from '../components/border';
-import { Navbar } from '../components/Navbar';
-import { timeAgo } from '../utils';
-import { getBorders } from '../utils';
+import { useState, useEffect } from "react";
+import Head from "next/head";
+import Border from "../components/border";
+import { Navbar } from "../components/Navbar";
+import { timeAgo } from "../utils";
+import { getBorders } from "../utils";
 
 export default function Home({ date, time, timeAgo, ports }) {
   const [filtered, setFiltered] = useState(ports || []);
 
   const [tags, setTags] = useState([]);
-  const [userCoordinates, setUserCoordinates] = useState({ lat: 0, lon: 0, msg: '' });
+  const [userCoordinates, setUserCoordinates] = useState({
+    lat: 0,
+    lon: 0,
+    msg: "",
+  });
 
   const handleInput = (e) => {
-    setTags(e)
+    setTags(e);
     let found = [];
     if (e.length === 0) {
       setFiltered(ports);
@@ -24,7 +28,12 @@ export default function Home({ date, time, timeAgo, ports }) {
         const crossing = port.crossing_name.toLowerCase();
         const number = port.port_number.toLowerCase();
         const border = port.border.toLowerCase();
-        return name.includes(tag) || crossing.includes(tag) || number.includes(tag) || border.includes(tag);
+        return (
+          name.includes(tag) ||
+          crossing.includes(tag) ||
+          number.includes(tag) ||
+          border.includes(tag)
+        );
       });
     });
     setFiltered(found);
@@ -37,11 +46,15 @@ export default function Home({ date, time, timeAgo, ports }) {
   };
   function success(pos) {
     var crd = pos.coords;
-    console.log('Your current position is:');
+    console.log("Your current position is:");
     console.log(`Latitude : ${crd.latitude}`);
     console.log(`Longitude: ${crd.longitude}`);
     console.log(`More or less ${crd.accuracy} meters.`);
-    setUserCoordinates({ lat: crd.latitude, lon: crd.longitude, msg: `More or less ${crd.accuracy} meters.` });
+    setUserCoordinates({
+      lat: crd.latitude,
+      lon: crd.longitude,
+      msg: `More or less ${crd.accuracy} meters.`,
+    });
   }
 
   function errors(err) {
@@ -51,21 +64,21 @@ export default function Home({ date, time, timeAgo, ports }) {
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.permissions
-        .query({ name: 'geolocation' })
-        .then(function(result) {
+        .query({ name: "geolocation" })
+        .then(function (result) {
           console.log(result);
-          if (result.state === 'granted') {
+          if (result.state === "granted") {
             //If granted then you can directly call your function here
             navigator.geolocation.getCurrentPosition(success, errors, options);
-          } else if (result.state === 'prompt') {
+          } else if (result.state === "prompt") {
             //If prompt then the user will be asked to give permission
             navigator.geolocation.getCurrentPosition(success, errors, options);
-          } else if (result.state === 'denied') {
+          } else if (result.state === "denied") {
             //If denied then you have to show instructions to enable location
           }
         });
     } else {
-      console.log('Geolocation is not supported by this browser.');
+      console.log("Geolocation is not supported by this browser.");
     }
   }, []);
 
@@ -73,29 +86,36 @@ export default function Home({ date, time, timeAgo, ports }) {
     <main>
       <Head>
         <title>Border Wait Times US</title>
-        <meta name='google' content='nositelinkssearchbox' key='sitelinks' />
-        <meta name='google' content='notranslate' key='notranslate' />
+        <meta name="google" content="nositelinkssearchbox" key="sitelinks" />
+        <meta name="google" content="notranslate" key="notranslate" />
       </Head>
-      <Navbar date={date} time={time} tags={tags} handleInput={handleInput} />
-      <div className='flex flex-col gap-4 p-20 bg-gray'>
-        <div className='flex flex-col items-end'>
-          <code className='text-sm font-light'>
-            Updated: {date} {time}
-          </code>
-          <code className='text-sm font-light'>{timeAgo}</code>
-        </div>
-        <div>
-          <div className='flex'>
-            <div className='grid xs:grid-cols-1 sm:grid-col-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2'>
-              {filtered.map((port) => (
-                <Border key={port.port__name + port.port_number} {...port} userCoordinates={userCoordinates} />
-              ))}
-            </div>
-          </div>
+      <Navbar
+        date={date}
+        time={time}
+        timeago={timeAgo}
+        tags={tags}
+        handleInput={handleInput}
+      />
+      <div className="flex flex-col gap-4 p-20 pt-6 bg-zinc-950 min-h-screen">
+        <div className="grid xs:grid-cols-1 sm:grid-col-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2">
+          {filtered.map((port) => (
+            <Border
+              key={port.port__name + port.port_number}
+              {...port}
+              userCoordinates={userCoordinates}
+            />
+          ))}
         </div>
       </div>
-      <div className='flex text-zinc-500 justify-center align-middle items-enter p-10'>
-        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16">
+      <div className="flex text-zinc-500 justify-center align-middle items-enter p-10">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="36"
+          height="36"
+          fill="currentColor"
+          className="bi bi-github"
+          viewBox="0 0 16 16"
+        >
           <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
         </svg>
       </div>
